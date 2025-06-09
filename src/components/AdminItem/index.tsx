31858541a034158a6bbf711f0625b8698a143106
ftery0 <lerb5253@dgsw.hs.kr>
2025-06-09 12:32:48 +0900

    feat :: admin page

--- src/components/AdminItem/index.tsx
+++ src/components/AdminItem/index.tsx
@@ -1,35 +1,72 @@
 import { DodamFilledButton } from "@b1nd/dds-web";
 import { AdminItemBox, ButtonBox, ContentBox, ImgBox, TitleBox } from "./style";
+import { Blog } from "../../types/Blog/blog.type";
+import { B1ndToast } from "@b1nd/b1nd-toastify";
+import { useApproveBlogMutation } from "../../queries/Blog/blog.query";
+import { useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
 
-const AdminItem = () => {
+const AdminItem = ({ data }: { data: Blog }) => {
+    const approveBlogMutation = useApproveBlogMutation();
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
 
-    return(
-        <AdminItemBox>
-            <ImgBox>
-                <img src="" alt="에러" />
-            </ImgBox>
-            <ContentBox>
-                <TitleBox>
-                    <span>제목입니다</span>
-                    <span>내용입니다</span>
-                </TitleBox>
-                <ButtonBox>
-                    <DodamFilledButton
-                        width={84}
-                        text="수락"
-                        backgroundColorType="Primary" 
-                        size="Large"
-                        />
-                      <DodamFilledButton
-                        width={84}
-                        text="거절"
-                        backgroundColorType="Assistive"
-                        size="Large"
-                    />
-                </ButtonBox>
-            </ContentBox>
-        </AdminItemBox>
-    )
-}
+    
+    const approveButton = () => {
+        approveBlogMutation.mutate(data.post_id, {
+            onSuccess: () => {
+                B1ndToast.showError("블로그 글이 수락되었습니다.")
+                queryClient.invalidateQueries("blogs/waiting");
+            },
+            onError: (error:any) => {
+                B1ndToast.showError(`블로그 글 수락에 실패했습니다: ${error.message}`);
+            },
+        });
+    }
+    const rejectButton = () => {
+        // approveBlogMutation.mutate(data.post_id, {
+        //     onSuccess: () => {
+        //         B1ndToast.showError("블로그 글이 거절되었습니다.")
+        //     },
+        //     onError: (error:any) => {
+        //         B1ndToast.showError(`블로그 글 거절에 실패했습니다: ${error.message}`);
+        //     },
+        // });
+    } 
+
+    const handleClick = () => {
+        navigate(`/blog/${data.post_id}`); 
+      };
+
+  return (
+    <AdminItemBox onClick={handleClick}>
+      <ImgBox>
+        {/* <img src={data? || ""} alt="썸네일" /> */}
+      </ImgBox>
+      <ContentBox>
+        <TitleBox>
+          <span>{data?.post_title}</span>
+          <span>{data?.post_content || "내용 요약이 없습니다"}</span>
+        </TitleBox>
+        <ButtonBox>
+          <DodamFilledButton
+            width={84}
+            text="수락"
+            backgroundColorType="Primary"
+            size="Large"
+            onClick={() => approveButton()}
+          />
+          <DodamFilledButton
+            width={84}
+            text="거절"
+            backgroundColorType="Assistive"
+            size="Large"
+            onClick={() => rejectButton()}
+          />
+        </ButtonBox>
+      </ContentBox>
+    </AdminItemBox>
+  );
+};
 
 export default AdminItem;
--- src/page/Admin/admin.tsx
+++ src/page/Admin/admin.tsx
@@ -1,12 +1,71 @@
-import AdminItem from "../../components/AdminItem";
-import { AdminBox } from "./style";
-
-const Admin = () => {
-    return(
-        <AdminBox>
-            <AdminItem/>
-        </AdminBox>
-    )
-}
-
-export default Admin;
+import {
+    BlogBox,
+    BlogContentBox,
+    BlogContentContainer,
+    PagiNation,
+    ToastText,
+  } from '../blog/style'; 
+  import { Title } from '../style';
+  import { DodamErrorBoundary } from '@b1nd/dds-web';
+  import AdminItem from '../../components/AdminItem';
+  import { useState } from 'react';
+  import { useBlogWaitingList } from '../../queries/Blog/blog.query';
+  
+  const Admin = () => {
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching } = useBlogWaitingList({ page });
+  
+    const totalPages = data?.totalPage || 1;
+  
+    const handlePrev = () => {
+      if (page > 1) setPage(prev => prev - 1);
+    };
+  
+    const handleNext = () => {
+      if (page < totalPages) setPage(prev => prev + 1);
+    };
+  
+    return (
+      <BlogBox>
+        <Title>
+          <h1>블로그 수락/거절 관리</h1>
+          <span>관리자는 등록 대기 중인 블로그 글을 수락하거나 거절할 수 있습니다.</span>
+        </Title>
+  
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <BlogContentContainer>
+            <BlogContentBox>
+              <DodamErrorBoundary text="에러 발생" showButton={true}>
+                {isLoading || isFetching ? (
+                  <ToastText>로딩중..</ToastText>
+                ) : data?.data.length === 0 ? (
+                  <ToastText>대기 중인 게시글이 없습니다.</ToastText>
+                ) : (
+                  <>
+                    {data?.data.map((item, index) => (
+                      <AdminItem key={index} data={item} />
+                    ))}
+                  </>
+                )}
+              </DodamErrorBoundary>
+            </BlogContentBox>
+  
+            <PagiNation>
+              <button disabled={page === 1} onClick={handlePrev}>
+                이전
+              </button>
+              <span>
+                {page} / {totalPages}
+              </span>
+              <button disabled={page === totalPages} onClick={handleNext}>
+                다음
+              </button>
+            </PagiNation>
+          </BlogContentContainer>
+        </div>
+      </BlogBox>
+    );
+  };
+  
+  export default Admin;
+  
